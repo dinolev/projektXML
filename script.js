67f0352ba9a9ae996f05da1509ab858ce2e81023
Dino <84812140+dinolev@users.noreply.github.com>
2021-05-28 16:07:06 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,244 @@
+
+var ekipa = [{
+    "ime": "Danijel",
+    "prezime": "Zagorac",
+    "broj": "1",
+    "pozicija": "vratar",
+    "bivskiKlubovi": "Split,Lokomotiva",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Zagorac_f6acdcf3-c28c-4876-ab5b-d66d757a10b9_330x330.jpg"
+}, {
+    "ime": "Sadegh",
+    "prezime": "Moharrami",
+    "broj": "2",
+    "pozicija": "Desni bočni",
+    "bivskiKlubovi": "Malavan,Persepolis,Lokomotiva",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Moharrami_d6839e77-18a2-49df-8520-9a38e2e91e7f_330x330.jpg"
+},{
+    "ime": "Stefan ",
+    "prezime": "Milić",
+    "broj": "4",
+    "pozicija": "Stoper",
+    "bivskiKlubovi": "Podgorica",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Milic_e08b23c9-d5c7-4515-833b-b6d84040b00b_330x330.jpg"
+},{
+    "ime": "Arijan",
+    "prezime": "Ademi",
+    "broj": "5",
+    "pozicija": "Defanzivni vezni",
+    "bivskiKlubovi": "Šibenik,Lokomotiva",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Ademi_a54abb34-4b37-4f94-8e65-0e935df2f2fe_330x330.jpg"
+}, {
+    "ime": "Rasmus",
+    "prezime": "Lauritsen",
+    "broj": "6",
+    "pozicija": "Stoper",
+    "bivskiKlubovi": "Midtylland, Skive, Vejle, Norrkoping",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/CopyofLauritsen_af208393-7588-4a1f-95a0-67f5d4ec545c_330x330.jpg" 
+},{
+    "ime": "Izet",
+    "prezime": "Hajrović",
+    "broj": "8",
+    "pozicija": "Krilo",
+    "bivskiKlubovi": "Zurich, Grassoppers, Galatasaray, Eibar, Werder Bremen",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Hajrovici_9452c0fc-b668-4979-bbc5-fba4af033211_330x330.jpg" 
+},{
+    "ime": "Lovro",
+    "prezime": "Majer",
+    "broj": "10",
+    "pozicija": "Ofenzivni vezni",
+    "bivskiKlubovi": "Dubrava,Trnje,Lokomotiva",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Majer_d30b954f-37c7-4e4b-8210-70f065b8ba52_330x330.jpg" 
+},
+{
+    "ime": "Mario",
+    "prezime": "Gavranović",
+    "broj": "11",
+    "pozicija": "Napadač",
+    "bivskiKlubovi": "Ticino, Lugano, Yverdon, XAMAX, Schalke, Mainz, Zurich, Rijeka",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Gavranovic_7cc107e2-7a67-4f3e-8d8c-0cafa4b8d5ea_330x330.jpg" 
+},
+{
+    "ime": "Stefan",
+    "prezime": "Ristovski",
+    "broj": "13",
+    "pozicija": "Desni bočni",
+    "bivskiKlubovi": "Spezia, Rijeka, Sporting CP",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Ristovski_e7cdbe33-3da1-41f1-a15b-a38701c4a3e5_330x330.jpg" 
+},
+{
+    "ime": "Luka",
+    "prezime": "Ivanušec",
+    "broj": "17",
+    "pozicija": "Vezni",
+    "bivskiKlubovi": "Varaždin, Lokomotiva",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Ivanusec_83761b48-4678-46da-8f0c-ed27216110c4_330x330.jpg" 
+},
+{
+    "ime": "Marijan",
+    "prezime": "Čabraja",
+    "broj": "19",
+    "pozicija": "Lijevi bočni",
+    "bivskiKlubovi": "HNK Gorica",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Cabraja_7fa4f6e1-849f-4cee-a651-3ce253f9a1a1_330x330.jpg" 
+},
+{
+    "ime": "Lirim",
+    "prezime": "Kastrati",
+    "broj": "20",
+    "pozicija": "Krilo",
+    "bivskiKlubovi": "Dardana, Shkendija Tirana, Lokomotiva",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Kastrati_1d5cb668-eaa6-46e9-bf4f-881e2a5845da_330x330.jpg" 
+},
+{
+    "ime": "Bruno",
+    "prezime": "Petković",
+    "broj": "21",
+    "pozicija": "Napadač",
+    "bivskiKlubovi": "Neretva, Zagreb, Hašk, Hrvatski Dragovoljac, Catania, Varese, Regigana, Virtus Entella, Trapani, Bologna, Verona",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Petkovic_75edb195-7c76-49c0-853c-6c3515efa3ff_330x330.jpg" 
+},
+{
+    "ime": "Marin",
+    "prezime": "Leovac",
+    "broj": "22",
+    "pozicija": "Lijevi bočni",
+    "bivskiKlubovi": "Austria Wien, Rijeka, Paok",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Leovac_aed5498d-4ede-4e7f-bc65-8711cd0142cd_330x330.jpg" 
+},
+{
+    "ime": "Marko",
+    "prezime": "Tolić",
+    "broj": "24",
+    "pozicija": "Vezni",
+    "bivskiKlubovi": "Croatia Sesvete, Lokomotiva, Hrvatski Dragovoljac, Lučko, Gorica",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Tolic_88c4bd1b-b326-43b8-8b31-d208913a022b_330x330.jpg" 
+},
+{
+    "ime": "Robert",
+    "prezime": "Burton",
+    "broj": "26",
+    "pozicija": "Vezni",
+    "bivskiKlubovi": "Arsenal",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Burton_d557e70b-a408-4743-83e9-0279469026ea_330x330.jpg" 
+},
+{
+    "ime": "Josip",
+    "prezime": "Mišić",
+    "broj": "27",
+    "pozicija": "Vezni",
+    "bivskiKlubovi": "Osijek, Rijeka, Spezia, Sporting, Paok",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Misisc_Josip_2726bdbd-56c7-4ade-8276-a6d7dcfae022_330x330.jpg" 
+},
+{
+    "ime": "Kevin",
+    "prezime": "Theophile-Catherine",
+    "broj": "28",
+    "pozicija": "Stoper",
+    "bivskiKlubovi": "Rennes, Cardiff city, Saint-Etienne",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Theophile_e64cad6d-77a3-41be-819d-3719e4f801be_330x330.jpg" 
+},
+{
+    "ime": "Petar",
+    "prezime": "Stojanović",
+    "broj": "30",
+    "pozicija": "Desni bočni",
+    "bivskiKlubovi": "Interblock, Veržej, Maribor",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Stojanovic_288823f9-787c-45cb-be6b-819e90585a4d_330x330.jpg" 
+},
+{
+    "ime": "Joško",
+    "prezime": "Gvardiol",
+    "broj": "32",
+    "pozicija": "Stoper",
+    "bivskiKlubovi": "Trešnjevka",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Gvardiol_adf252a0-91e2-45ce-a16a-19e889454fed_330x330.jpg" 
+},
+{
+    "ime": "Renato",
+    "prezime": "Josipović",
+    "broj": "33",
+    "pozicija": "Vratar",
+    "bivskiKlubovi": "Šibenik",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Josipovic_47d583a5-a01a-4520-b140-f40050efc2ec_330x330.jpg" 
+},
+{
+    "ime": "Bartol",
+    "prezime": "Franjić",
+    "broj": "38",
+    "pozicija": "Defenzivno vezni",
+    "bivskiKlubovi": "",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Franjic_1aae9108-aab3-4b33-a274-7c5c68f0b0d9_330x330.jpg" 
+},
+{
+    "ime": "Dominik",
+    "prezime": "Livaković",
+    "broj": "40",
+    "pozicija": "Vratar",
+    "bivskiKlubovi": "Zadar, Zagreb",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Livakovic_9889f653-2027-40e3-b273-352e004a437b_330x330.jpg" 
+},
+{
+    "ime": "Dino",
+    "prezime": "Perić",
+    "broj": "55",
+    "pozicija": "Stoper",
+    "bivskiKlubovi": "Osijek, Sesvete, Lokomotiva",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Peric_42bbf8ef-095d-4791-9af5-ea6b951fae20_330x330.jpg" 
+},
+{
+    "ime": "Iyayi",
+    "prezime": "Atiemwen",
+    "broj": "80",
+    "pozicija": "Vezni",
+    "bivskiKlubovi": "Bendel, Dogan, Kayseri, Gorica, Lokomotiva",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Atiemwen_7b760b8b-9fef-4f9b-911f-0c228ba983e8_330x330.jpg" 
+},
+{
+    "ime": "Kristijan",
+    "prezime": "Jakić",
+    "broj": "97",
+    "pozicija": "Defenzivni vezni",
+    "bivskiKlubovi": "Imotski, Split, Lokomotiva, Istra 1961",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Jaki%C4%87_5273004e-dab2-4e02-baff-44ced0b4fbda_330x330.jpg" 
+},
+{
+    "ime": "Mislav",
+    "prezime": "Oršić",
+    "broj": "99",
+    "pozicija": "Krilo",
+    "bivskiKlubovi": "Kustošija, Inter Zaprešić, Spezia, Rijeka, Celje, Jeonnam Dragons, Ulsan Hyundai, Changchun  Yatai",
+    "photo" : "https://dinamo.blob.core.windows.net/dinamo/Orsic_cc11005e-98c6-43bc-a0b3-127262e1efd1_330x330.jpg" 
+},
+]
+
+
+
+kreirajTablicu(ekipa)
+
+function kreirajTablicu(data) {
+    var tablica = document.getElementById('mojaTablica')
+
+    for(var i = 0; i < data.length; i++) {
+        var row = 
+        `<tr>
+            <td><img onmouseover="ulaz(this)" onmouseout="izlaz(this)" src="${data[i].photo}" width="200"></td>
+            <td>${data[i].ime}</td>
+            <td>${data[i].prezime}</td>
+            <td>${data[i].broj}</td>
+            <td>${data[i].pozicija}</td>
+            <td>${data[i].bivskiKlubovi}</td>
+        </tr>`
+        tablica.innerHTML += row
+    }
+}
+
+
+function ulaz(x) {
+    x.style.height = "330px";
+    x.style.width = "330px";
+  }
+
+  function izlaz(x) {
+    x.style.height = "200px";
+    x.style.width = "200px";
+  }
